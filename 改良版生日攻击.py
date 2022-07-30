16c54f96187c0f7973137c9982998901f2e42489
opaofruitmilk <75268355+opaofruitmilk@users.noreply.github.com>
2022-07-30 15:24:25 +0800

    Update 改良版生日攻击.py

--- 改良版生日攻击.py
+++ 改良版生日攻击.py
@@ -14,16 +14,19 @@ from gmssl import sm3, func
 
 def improved_birthday_attack(l):
     m=2**l
-    x=random.randint(0,m).to_bytes(4, byteorder='little', signed=True)
+    x=random.randint(0,m).to_bytes(1, byteorder='little', signed=True)
+    print(x)
     x1=x;x2=x;i=0
     while(1):
         x1=bytes(sm3.sm3_hash(func.bytes_to_list(x1)),encoding ="utf8")
         x2=bytes(sm3.sm3_hash(func.bytes_to_list(x2)),encoding ="utf8")
         x2=bytes(sm3.sm3_hash(func.bytes_to_list(x2)),encoding ="utf8")
         i+=1
+        print(x1,x2)
         if x1 == x2: break
     x2=x1;x1=x
-    for i in range (1,i):
+    print(1)
+    for j in range (1,i):
         if sm3.sm3_hash(func.bytes_to_list(x1))==sm3.sm3_hash(func.bytes_to_list(x2)):
             return (x1,x2)
             break
